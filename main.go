62fc60a0261d25caa75e2bdb1cb16d9afd276228
John Smith <john@johnjcsmith.com>
2024-10-04 08:35:41 +0930

    feat: Update demo to use order example (#1)
    * feat: Update demo to use order example
    
    * fix: Correct OrderRequest struct to use InventoryItems (#2)
    
    ---------
    
    Co-authored-by: Nadeesha Cabral <n@nadeesha.me>

--- main.go
+++ main.go
@@ -1,64 +1,94 @@
 package main
 
 import (
-	"os"
+  "os"
 
-	"github.com/inferablehq/inferable-go"
-	"github.com/joho/godotenv"
+  "github.com/inferablehq/inferable-go"
+  "github.com/joho/godotenv"
 )
 
 func main() {
-	// Load vars from .env file
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
+  // Load vars from .env file
+  err := godotenv.Load()
+  if err != nil {
+    panic(err)
+  }
 
-	// Instantiate the Inferable client.
-	client, err := inferable.New(inferable.InferableOptions{
-		// To get a new key, run:
-		// npx @inferable/cli auth keys create 'My New Machine Key' --type='cluster_machine'
-		APISecret:   os.Getenv("INFERABLE_API_SECRET"),
-		APIEndpoint: os.Getenv("INFERABLE_API_ENDPOINT"),
-	})
+  // Instantiate the Inferable client.
+  client, err := inferable.New(inferable.InferableOptions{
+    // To get a new key, run:
+    // npx @inferable/cli auth keys create 'My New Machine Key' --type='cluster_machine'
+    APISecret:   os.Getenv("INFERABLE_API_SECRET"),
+    APIEndpoint: os.Getenv("INFERABLE_API_ENDPOINT"),
+  })
 
-	if err != nil {
-		panic(err)
-	}
+  if err != nil {
+    panic(err)
+  }
 
-	service, err := client.RegisterService("MyService")
-	if err != nil {
-		// Handle error
-	}
+  service, err := client.RegisterService("MyService")
+  if err != nil {
+    // Handle error
+  }
 
-	type MyInput struct {
-		Message string `json:"message"`
-	}
+  // Register demo functions (Defined in ./src/demo.go)
+  err = service.RegisterFunc(inferable.Function{
+    Func:        SearchInventory,
+    Name:        "searchInventory",
+    Description: "Searches the inventory",
+  })
+  if err != nil {
+    panic(err)
+  }
 
-	myFunc := func(input MyInput) (string, error) {
-		return "Hello, " + input.Message, nil
-	}
+  err = service.RegisterFunc(inferable.Function{
+    Func:        GetInventoryItem,
+    Name:        "getInventoryItem",
+    Description: "Gets an inventory item",
+  })
+  if err != nil {
+    panic(err)
+  }
 
-	// Register a demo function
-	err = service.RegisterFunc(inferable.Function{
-		Func:        myFunc,
-		Name:        "MyFunction",
-		Description: "A simple greeting function",
-	})
+  err = service.RegisterFunc(inferable.Function{
+    Func:        ListOrders,
+    Name:        "listOrders",
+    Description: "Lists all orders",
+  })
+  if err != nil {
+    panic(err)
+  }
 
-	if err != nil {
-		panic(err)
-	}
+  err = service.RegisterFunc(inferable.Function{
+    Func:        TotalOrderValue,
+    Name:        "totalOrderValue",
+    Description: "Calculates the total value of all orders",
+  })
+  if err != nil {
+    panic(err)
+  }
 
-	service.Start()
+  type OrderConfig struct {
+    RequiresApproval bool
+  }
 
-	err = service.Start()
-	if err != nil {
-		panic(err)
-	}
+  err = service.RegisterFunc(inferable.Function{
+    Func:        MakeOrder,
+    Name:        "makeOrder",
+    Description: "Makes an order",
+    Config:       OrderConfig{RequiresApproval: true},
+  })
+  if err != nil {
+    panic(err)
+  }
 
-	defer service.Stop()
+  err = service.Start()
+  if err != nil {
+    panic(err)
+  }
 
-	// Wait for CTRL+C
-	<-make(chan struct{})
+  defer service.Stop()
+
+  // Wait for CTRL+C
+  <-make(chan struct{})
 }
